24bf2e4de5d46b13789f797b231ce3892b353524
GuRuGu <peter.k.nets@gmail.com>
2020-02-18 16:15:52 +0200

    Add feed root field and Link type

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 const { GraphQLServer } = require("graphql-yoga");
 
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
 // 1
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
 
 // 2
 const resolvers = {
   Query: {
-    // info: () => `This is the API of a Hackernews Clone`
-    info: () => null
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
   }
 };
 
