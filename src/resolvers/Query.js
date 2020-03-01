26c8287c0eeebf822602fb2708bb746802f45078
GuRuGu <peter.k.nets@gmail.com>
2020-03-01 06:50:28 +0200

    Implement count

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -15,7 +15,12 @@ async function feed(parent, args, context, info) {
     orderBy: args.orderBy
   });
 
-  return links;
+  const count = await context.prisma
+    .linksConnection({ where })
+    .aggregate()
+    .count();
+
+  return { links, count };
 }
 
 module.exports = {
